d19b53a3e5060499436bfca9ba43249fc4796610
roniba1 <roniba16@gmail.com>
2023-01-10 15:18:36 +0200

    Forecast logic
    Forecast logic working

--- src/pages/ForecastPage.tsx
+++ src/pages/ForecastPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Descriptions, Button } from 'antd'
+import {Descriptions, Image, Select} from 'antd'
 
 interface WeatherData {
     current: {
@@ -20,38 +20,37 @@ interface WeatherData {
 
 const API_KEY = '7830b3a060d8739ac5d8271370d2f78d';
 const UNITS = 'metric';
-const OPTIONS = [
-    "home",
-    "kindergarten",
-    "work"
-];
 
 interface LocationData {
+    value: string;
     label: string;
     latitude: number;
     longitude: number;
 }
-const LOCATIONS = {
-    home: {
+const LOCATIONS = [
+    {
+        value: "home",
         label: "Home",
         latitude: 32.33,
         longitude: 34.95
     },
-    kindergarten: {
+    {
+        value: "kindergarten",
         label: "Kindergarten",
         latitude: 32.36,
         longitude: 34.92
     },
-    work: {
+    {
+        value: "work",
         label: "Work",
         latitude: 32.27,
         longitude: 34.85
     }
-};
+];
 
 const ForecastPage: React.FC = () => {
     const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-    const [locationData, setLocationData] = useState<LocationData>(LOCATIONS.home);
+    const [locationData, setLocationData] = useState<LocationData>(LOCATIONS[0]);
 
     const getUrl = () => {
         return `https://api.openweathermap.org/data/3.0/onecall?lat=${locationData.latitude}&lon=${locationData.longitude}&units=${UNITS}&appid=${API_KEY}`
@@ -59,36 +58,74 @@ const ForecastPage: React.FC = () => {
 
     const title = "Current Forecast - " + locationData.label;
 
+    const options = LOCATIONS.map((locationData) => {
+            return {
+                value: locationData.value,
+                label: locationData.label
+            };
+        });
+
     const fetchData = async() => {
         const res = await fetch(getUrl());
         const data = await res.json();
         setWeatherData(data);
-    }
+    };
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [locationData]);
+
+    const handleSelectChange = (value: string) => {
+        const newLocation = LOCATIONS.find((locationData) => {
+            return locationData.value === value;
+        });
+        if (newLocation) {
+            setLocationData(newLocation);
+        }
+    };
+
+    const selector = (
+        <Select
+        placeholder="Choose category"
+        value={locationData.label}
+        onChange={handleSelectChange}
+        options={options}
+        style={{
+            width: 130
+        }}
+    />);
+
+    const description = <>
+        <Image
+            width={25}
+            preview={false}
+            src={`http://openweathermap.org/img/wn/${weatherData?.current.weather[0].icon}@2x.png`}
+        />
+        {weatherData?.current.weather[0].description}
+    </>;
+
 
     if (weatherData === null) {
         return <div>Loading weather data...</div>;
     }
 
-    return (
-        <div style={{margin:"25px"}}>
-            <Descriptions
-                bordered
-                title={title}
-                extra={<Button type="primary">Edit</Button>}>
-                <Descriptions.Item label="Description">{weatherData.current.weather[0].description}</Descriptions.Item>
-                <Descriptions.Item label="Temperature">{weatherData.current.temp}</Descriptions.Item>
-                <Descriptions.Item label="Feels like">{weatherData.current.feels_like}</Descriptions.Item>
-                <Descriptions.Item label="Humidity">{weatherData.current.humidity}%</Descriptions.Item>
-                <Descriptions.Item label="UV index">{weatherData.current.uvi}</Descriptions.Item>
-                <Descriptions.Item label="Wind speed">{weatherData.current.wind_speed}</Descriptions.Item>
-                <Descriptions.Item label="Cloudiness">{weatherData.current.clouds}%</Descriptions.Item>
-            </Descriptions>
-        </div>
-    );
+        return (
+            <div style={{margin:"25px"}}>
+                <Descriptions
+                    bordered
+                    title={title}
+                    extra={selector}>
+                    <Descriptions.Item label="Description">{description}</Descriptions.Item>
+                    <Descriptions.Item label="Temperature">{weatherData.current.temp}</Descriptions.Item>
+                    <Descriptions.Item label="Feels like">{weatherData.current.feels_like}</Descriptions.Item>
+                    <Descriptions.Item label="Humidity">{weatherData.current.humidity}%</Descriptions.Item>
+                    <Descriptions.Item label="UV index">{weatherData.current.uvi}</Descriptions.Item>
+                    <Descriptions.Item label="Wind speed">{weatherData.current.wind_speed}</Descriptions.Item>
+                    <Descriptions.Item label="Cloudiness">{weatherData.current.clouds}%</Descriptions.Item>
+                </Descriptions>
+            </div>
+        );
+
 }
 
 export default ForecastPage;
